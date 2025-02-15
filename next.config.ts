d1cf92323eda0182add2573d6c9b8a9334754b2c
jonasostrom <jonas.ostrom@gmail.com>
2025-02-15 21:18:42 +0100

    add basepath to conf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import { config } from 'dotenv';
 
+config({ path: './.env' });
+
+const basePath = process.env.BASE_PATH || '';
+console.log('basePath:', basePath);
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath,
 };
 
 export default nextConfig;
