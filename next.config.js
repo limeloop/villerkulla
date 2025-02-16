841b8c0df2fa2b194cf223f7542e08cf99ddcfd5
jonasostrom <jonas.ostrom@gmail.com>
2025-02-16 12:49:17 +0100

    try add rewrites

--- next.config.js
+++ next.config.js
@@ -2,16 +2,31 @@
 
 // config({ path: './.env' });
 
-const basePath = process.env.BASE_PATH || '';
-
-console.log('basePath from env:', basePath);
-
-const nextConfig = {
-  /* config options here */
-  basePath: '/my-app-test',
-  assetPrefix: '/my-app-test/',
-};
-console.log('nextConfig:', nextConfig);
-
-module.exports = nextConfig;
-
+const BASE_PREFIX_FOR_APP = process.env.BASE_PATH || '';
+console.log('BASE_PREFIX_FOR_APP', BASE_PREFIX_FOR_APP);
+// next.config.js
+const conf = {
+  basePath: BASE_PREFIX_FOR_APP,
+  assetPrefix: BASE_PREFIX_FOR_APP+'/',
+  async rewrites(){
+    return [
+      {
+        /** ASSET PREFIX */
+        source: `${BASE_PREFIX_FOR_APP}/_next/:path*`,
+        destination: '/_next/:path*'
+      },
+      {
+        /** IMAGE PREFIX */
+        source: `${BASE_PREFIX_FOR_APP}/images/:query*`,
+        destination: '/_next/image/:query*'
+      },
+      /** API PREFIX */
+      {
+        source: `${BASE_PREFIX_FOR_APP}/api/:path*`,
+        destination: '/api/:path*'
+      }
+    ]
+  }
+}
+console.log(conf);
+module.exports = conf;
