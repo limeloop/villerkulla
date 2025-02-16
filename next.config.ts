d17cac62bdcec089ef9de40dc70d92f1a79f7839
jonasostrom <jonas.ostrom@gmail.com>
2025-02-16 20:00:53 +0100

    strip next config

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,38 @@
 import type { NextConfig } from 'next';
 
-const BASE_PREFIX_FOR_APP: string = process.env.BASE_PATH || '';
+// const BASE_PREFIX_FOR_APP: string = process.env.BASE_PATH || '';
 
-console.log('BASE_PREFIX_FOR_APP', BASE_PREFIX_FOR_APP);
+// console.log('BASE_PREFIX_FOR_APP', BASE_PREFIX_FOR_APP);
 
-interface Rewrite {
-  source: string;
-  destination: string;
-}
+// interface Rewrite {
+//   source: string;
+//   destination: string;
+// }
 
 const nextConfig: NextConfig = {
-  basePath: BASE_PREFIX_FOR_APP,
-  assetPrefix: BASE_PREFIX_FOR_APP + '/',
-  async rewrites(): Promise<Rewrite[]> {
-    return [
-      {
-        // ASSET PREFIX
-        source: `${BASE_PREFIX_FOR_APP}/_next/:path*`,
-        destination: '/_next/:path*',
-      },
-      {
-        // IMAGE PREFIX
-        source: `${BASE_PREFIX_FOR_APP}/images/:query*`,
-        destination: '/_next/image/:query*',
-      },
-      {
-        // API PREFIX
-        source: `${BASE_PREFIX_FOR_APP}/api/:path*`,
-        destination: '/api/:path*',
-      },
-    ];
-  },
+  // basePath: BASE_PREFIX_FOR_APP,
+  // assetPrefix: BASE_PREFIX_FOR_APP + '/',
+  // async rewrites(): Promise<Rewrite[]> {
+  //   return [
+  //     {
+  //       // ASSET PREFIX
+  //       source: `${BASE_PREFIX_FOR_APP}/_next/:path*`,
+  //       destination: '/_next/:path*',
+  //     },
+  //     {
+  //       // IMAGE PREFIX
+  //       source: `${BASE_PREFIX_FOR_APP}/images/:query*`,
+  //       destination: '/_next/image/:query*',
+  //     },
+  //     {
+  //       // API PREFIX
+  //       source: `${BASE_PREFIX_FOR_APP}/api/:path*`,
+  //       destination: '/api/:path*',
+  //     },
+  //   ];
+  // },
 };
 
-console.log(nextConfig);
+// console.log(nextConfig);
 
 export default nextConfig;
